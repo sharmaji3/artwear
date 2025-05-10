ace7ef20daedf0e68a1b7203431eab4133c79496
sharmaji3 <176010899+sharmaji3@users.noreply.github.com>
2025-05-10 19:23:27 +0530

    auth complete

--- assets/custom.js
+++ assets/custom.js
@@ -1,11 +1,12 @@
- function togglePassword() {
-    const passwordField = document.getElementById('CustomerPassword');
-    const icon = document.getElementById('toggleIcon');
 
-    const isPassword = passwordField.type === 'password';
-    passwordField.type = isPassword ? 'text' : 'password';
+  function togglePassword(inputId, iconElement) {
+  const passwordField = document.getElementById(inputId);
 
-    // Toggle icon class
-    icon.classList.toggle('fa-eye');
-    icon.classList.toggle('fa-eye-slash');
-  }
+  if (!passwordField) return;
+
+  const isPassword = passwordField.type === 'password';
+  passwordField.type = isPassword ? 'text' : 'password';
+
+  iconElement.classList.toggle('fa-eye');
+  iconElement.classList.toggle('fa-eye-slash');
+}
